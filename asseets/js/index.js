bcb5a7b4267ed0962b17aa859cea1b39268e7e2a
Russell Frankpitt <75209707+rustyff12@users.noreply.github.com>
2024-09-26 14:08:24 +0200

    Getting form data and html outputs linked in js

--- asseets/js/index.js
+++ asseets/js/index.js
@@ -1,1 +1,21 @@
-// console.log("Testing");
+const apiKey = "FXBNTWCHC23MCDVMQYZ8DVM9F"; // lol
+
+const formSelection = document.querySelector("#location-selection");
+
+formSelection.addEventListener("submit", (e) => {
+	e.preventDefault();
+	console.log(e);
+	const country = document.getElementById("country").value;
+	const city = document.getElementById("city").value;
+
+	let currTemp = document.querySelector("#temp").innerText;
+	const dailyHigh = document.querySelector("#daily-high").innerText;
+	const dailyLow = document.querySelector("#daily-low").innerText;
+
+	console.log(currTemp);
+	console.log(dailyHigh);
+	console.log(dailyLow);
+
+	currTemp += " hello";
+	console.log(currTemp);
+});
